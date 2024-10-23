ee9e48784a0aa9c8ee6f03859907aa10edbc7651
BrettPost <bwpost@mtu.edu>
2024-10-22 22:05:11 -0700

    Selected Page Highlighting in the nav finished

--- src/react-portfolio/src/Components/Nav/Navbar.js
+++ src/react-portfolio/src/Components/Nav/Navbar.js
@@ -1,15 +1,22 @@
-import {Routes, Link, Route} from 'react-router-dom';
+import { useState } from 'react';
+import {Link} from 'react-router-dom';
 
 function Navbar(props) {
+    const [page, setPage] = useState("Home");
+
+    const changePage = (newPage) => {
+        setPage(newPage);
+    }
+
     return (
         <nav>
-            <Link to='/' className='title'>Sandbox</Link>
+            <Link to='/' onClick={(e) => changePage("Home")} className="title">Sandbox</Link>
             <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/Projects'>Projects</Link></li>
-                <li><Link to='/About'>About</Link></li>
+                {/* CONDENSE THIS INTO A LOOP */}
+                <li><Link to='/' onClick={(e) => changePage(e.target.text)} className={page === "Home" ? "selectedPageLink" : ""}>Home</Link></li>
+                <li><Link to='/Projects' onClick={(e) => changePage(e.target.text)} className={page === "Projects" ? "selectedPageLink" : ""}>Projects</Link></li>
+                <li><Link to='/About' onClick={(e) => changePage(e.target.text)} className={page === "About" ? "selectedPageLink" : ""}>About</Link></li>
             </ul>
-            
         </nav>
     )
 }
